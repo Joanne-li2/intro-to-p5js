16955c7fded99fbea3fec159efa6a8149bb1c7ae
JL <109628849+Joanne-li2@users.noreply.github.com>
2023-05-24 19:55:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,20 +6,26 @@ function setup() {
     canvas.position(110, 250);
     video = createCapture(VIDEO);
     video.hide();
-
-    tint_color = "";
 }
 
 function draw() {
     image(video, 0, 0, 640, 480);
-    tint(tint_color);
+    fill("blue")
+    stroke("red")
+    circle(50,50,70)
+    circle(600,50,70)
+    circle(600,450,70)
+    circle(50,450,70)
+
+    fill("pink")
+    stroke("orange")
+    rect(90,40,470,20)
+    rect(90,450,470,20)
+    rect(40,90,20,320)
+    rect(600,90,20,320)
 }
+
 
 function take_snapshot(){
     save('student_name.png');
-}
-
-function filter_tint()
-{
-    tint_color = document.getElementById("color_name").value
 }
